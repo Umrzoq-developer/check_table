3a1507b06a2595c055611fc3326d8ef3877638ac
Umrzoq Toshkentov <utoshkentov@gmail.com>
2020-08-27 13:40:41 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {connect} from 'react-redux';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+
+    return (
+        <div className="container">
+            <div className='row'>
+                {/*Main Table*/}
+                <div className="col-md-6">
+
+                </div>
+
+                {/*Changes Table*/}
+                <div className="col-md-6">
+                  {/*Obedinet Table*/}
+
+
+                  {/*Korzinka Table*/}
+
+                </div>
+
+            </div>
+        </div>
+    );
+};
 
 export default App;
