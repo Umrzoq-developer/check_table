0b0ea9bfe185ceae767986de41b3189f1b7280b5
Umrzoq Toshkentov <utoshkentov@gmail.com>
2020-08-27 19:46:53 +0500

    added

--- src/components/KorzinkaTable.js
+++ src/components/KorzinkaTable.js
@@ -7,39 +7,39 @@ const KorzinkaTable = ({korzinka, obidinet}) => {
         <div className='mt-5'>
             <h1>Korzinka: </h1>
             {
-                (korzinka.lengt > 0 || obidinet.length>0) ?
-                <div className='table table-active'>
-                    <thead>
-                    <th>Id:</th>
-                    <th>Name:</th>
-                    </thead>
-                    <tbody>
-                    {korzinka.map(item => {
-                        return (
-                            <tr key={item.id}>
-                                <td className='w-25'>
-                                    {item.id}
-                                </td>
-                                <td className='w-25'>
-                                    {item.name}
-                                </td>
-                            </tr>
-                        )
-                    })}
-                    {obidinet.map(item => {
-                        return (
-                            <tr key={item.id}>
-                                <td className='w-25'>
-                                    {item.id}
-                                </td>
-                                <td className='w-25'>
-                                    {item.name}
-                                </td>
-                            </tr>
-                        )
-                    })}
-                    </tbody>
-                </div> : (<p className='text-warning text-center'>There is not data</p>)
+                korzinka.length > 0 ? (
+                    <div className='table table-active'>
+                        <thead>
+                        <th>Id:</th>
+                        <th>Name:</th>
+                        </thead>
+                        <tbody>
+                        {korzinka.map(item => {
+                            return (
+                                <tr key={item.id}>
+                                    <td className='w-25'>
+                                        {item.id}
+                                    </td>
+                                    <td className='w-25'>
+                                        {item.name}
+                                    </td>
+                                </tr>
+                            )
+                        })}
+                        {obidinet.map(item => {
+                            return (
+                                <tr key={item.id}>
+                                    <td className='w-25'>
+                                        {item.id}
+                                    </td>
+                                    <td className='w-25'>
+                                        {item.name}
+                                    </td>
+                                </tr>
+                            )
+                        })}
+                        </tbody>
+                    </div>) : (<p className='text-warning text-center'>There is not data</p>)
             }
         </div>
     )
